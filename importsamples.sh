f1becc25aa09789c0af17d863a554c4130410970
cnapun <cnapun@gmail.com>
2016-07-20 16:03:25 -0700

    Add sample import

---
+++ importsamples.sh
@@ -0,0 +1,4 @@
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 1" -f samples/sample_pipelines/lesson1.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 2" -f samples/sample_pipelines/lesson2.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 3" -f samples/sample_pipelines/Lesson3.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 4" -f samples/sample_pipelines/Lesson4.json

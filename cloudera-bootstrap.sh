a990371b97ddaba55e7cf3be8a85e09b5296d89d
cnapun <cnapun@gmail.com>
2016-07-21 11:13:39 -0700

    disable tty

--- cloudera-bootstrap.sh
+++ cloudera-bootstrap.sh
@@ -1,2 +1,2 @@
-sudo docker exec -it $(sudo docker ps|tail -n1|head -c12) /home/cloudera/cloudera-manager --express
+sudo docker exec -i $(sudo docker ps|tail -n1|head -c12) /home/cloudera/cloudera-manager --express
 python clouderamanager-bootstrap.py

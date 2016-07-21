b786bd563b2fd6b93fa56a8d41da8b8d0cf4c532
Kirit Basu <kiritb@gmail.com>
2016-07-20 22:50:53 -0700

    underscores in pipeline names

--- importsamples.sh
+++ importsamples.sh
@@ -1,4 +1,4 @@
-streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 1" -f samples/sample_pipelines/lesson1.json
-streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 2" -f samples/sample_pipelines/lesson2.json
-streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 3" -f samples/sample_pipelines/Lesson3.json
-streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lesson 4" -f samples/sample_pipelines/Lesson4.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1" -f samples/sample_pipelines/lesson1.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2" -f samples/sample_pipelines/lesson2.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3" -f samples/sample_pipelines/Lesson3.json
+streamsets-datacollector-1.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4" -f samples/sample_pipelines/Lesson4.json

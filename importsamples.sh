d313be4eb2882c3ec92d70c328ea92e58b7954c9
Kirit Basu <kiritb@gmail.com>
2016-07-27 00:02:03 -0700

    samples case

--- importsamples.sh
+++ importsamples.sh
@@ -1,8 +1,8 @@
-streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_1" -f samples/sample_pipelines/lesson1_1.json
-streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_2" -f samples/sample_pipelines/lesson1_2.json
+streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_1" -f samples/sample_pipelines/Lesson1_1.json
+streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_2" -f samples/sample_pipelines/Lesson1_2.json
 streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_1" -f samples/sample_pipelines/Lesson2_1.json
 streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_2" -f samples/sample_pipelines/Lesson2_2.json
-streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_1" -f samples/sample_pipelines/lesson3_1.json
-streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_2" -f samples/sample_pipelines/lesson3_2.json
+streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_1" -f samples/sample_pipelines/Lesson3_1.json
+streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_2" -f samples/sample_pipelines/Lesson3_2.json
 streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1" -f samples/sample_pipelines/Lesson4_1.json
 streamsets-datacollector-1.5.1.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_2" -f samples/sample_pipelines/Lesson4_2.json
